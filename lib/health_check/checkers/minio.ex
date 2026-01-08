255710ef90e36cd76c553dbf356543803788463d
dzakurakin <dzakurakin@edenlab.com.ua>
2026-01-08 19:39:42 +0100

    fix: run

--- lib/health_check/checkers/minio.ex
+++ lib/health_check/checkers/minio.ex
@@ -3,36 +3,44 @@ defmodule HealthCheck.Checkers.Minio do
   require Logger
 
   def check(config \\ []) do
-    if Code.ensure_loaded?(ExAws.S3) and
-         Enum.member?(Application.started_applications() |> Enum.map(&elem(&1, 0)), :ex_aws) and
-         has_aws_keys?() do
-      bucket = config[:bucket] || System.get_env("MINIO_HEALTH_CHECK_BUCKET") || "health-check"
-
-      try do
-        case bucket |> ExAws.S3.list_objects() |> ExAws.request() do
-          {:ok, _} ->
-            :ok
-
-          {:error, {:http_error, 404, _}} ->
-            # Bucket not found might be acceptable if we just want to check connectivity
-            # but let's try to list buckets instead if no bucket is provided
-            check_connectivity()
-
-          {:error, reason} ->
-            Logger.error("Minio health check failed for bucket #{bucket}: #{inspect(reason)}")
-            {:error, :minio_error}
-        end
-      rescue
-        e ->
-          Logger.error("Minio health check failed: #{inspect(e)}")
-          {:error, :minio_exception}
-      catch
-        kind, reason ->
-          Logger.error("Minio health check failed: #{inspect({kind, reason})}")
-          {:error, :minio_exception}
-      end
+    if minio_active?() do
+      perform_check(config)
     else
       :ok
+    end
+  end
+
+  defp minio_active? do
+    Code.ensure_loaded?(ExAws.S3) and
+      Enum.any?(Application.started_applications(), fn {app, _, _} -> app == :ex_aws end) and
+      has_aws_keys?()
+  end
+
+  defp perform_check(config) do
+    bucket = config[:bucket] || System.get_env("MINIO_HEALTH_CHECK_BUCKET") || "health-check"
+
+    try do
+      case bucket |> ExAws.S3.list_objects() |> ExAws.request() do
+        {:ok, _} ->
+          :ok
+
+        {:error, {:http_error, 404, _}} ->
+          # Bucket not found might be acceptable if we just want to check connectivity
+          # but let's try to list buckets instead if no bucket is provided
+          check_connectivity()
+
+        {:error, reason} ->
+          Logger.error("Minio health check failed for bucket #{bucket}: #{inspect(reason)}")
+          {:error, :minio_error}
+      end
+    rescue
+      e ->
+        Logger.error("Minio health check failed: #{inspect(e)}")
+        {:error, :minio_exception}
+    catch
+      kind, reason ->
+        Logger.error("Minio health check failed: #{inspect({kind, reason})}")
+        {:error, :minio_exception}
     end
   end
 
