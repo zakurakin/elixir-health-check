86d98fbfad91f0a30e690800fc487078f45dbd95
dzakurakin <dzakurakin@edenlab.com.ua>
2026-01-08 19:45:09 +0100

    fix: run

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule HealthCheck.MixProject do
       {:ex_aws_s3, "~> 2.0", optional: true},
       {:hackney, "~> 1.9", optional: true},
       {:sweet_xml, "~> 0.6", optional: true},
-      {:ex_doc, "~> 0.32", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.32", runtime: false},
       {:excoveralls, "~> 0.18", only: [:dev, :test]},
       {:credo, "~> 1.7", runtime: false}
     ]
